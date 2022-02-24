e04fc34d00292172876450964fe13f5b4399336c
Nguyễn Thành Lâm <lnguyen24794@gmail.com>
2022-02-24 11:09:24 +0700

    Update tiktok.blade.php

--- resources/views/home/channel/tiktok.blade.php
+++ resources/views/home/channel/tiktok.blade.php
@@ -81,33 +81,33 @@
                     <table id="customers" style="margin-top: 10px;">
                         <thead>
                             <tr>
-                                <th>{{$values[0][0]}}</th>
-                                <th>{{$values[0][1]}}</th>
-                                <th>{{$values[0][2]}}</th>
-                                <th style="display: none">{{$values[0][3]}}</th>
-                                <th>{{$values[0][4]}}</th>
-                                <th>{{$values[0][7]}}</th>
-                                <th>{{$values[0][5]}}</th>
-                                <th>{{$values[0][6]}}</th>
+                                <th>{{$values[0][0]??''}}</th>
+                                <th>{{$values[0][1]??''}}</th>
+                                <th>{{$values[0][2]??''}}</th>
+                                <th style="display: none">{{$values[0][3]??''}}</th>
+                                <th>{{$values[0][4]??''}}</th>
+                                <th>{{$values[0][7]??''}}</th>
+                                <th>{{$values[0][5]??''}}</th>
+                                <th>{{$values[0][6]??''}}</th>
                             </tr>
                         </thead>
 
                         <tbody>
                             @for ($i = 1; $i < count($values)-1; $i++)
                                 <tr>
-                                    <td>{{$values[$i][0]}}</td>
+                                    <td>{{$values[$i][0]??''}}</td>
                                     <td>
                                         {{number_format($values[$i][1], 0, ',', '.')}}
                                     </td>
                                     <td style="display: none">
-                                        {{$values[$i][3]}}
+                                        {{$values[$i][3]??''}}
                                     </td>
                                     <td>
-                                        {{$values[$i][4]}}
+                                        {{$values[$i][4]??''}}
                                     </td>
                                     <td>
-                                        <input value="{{$values[$i][3]}}" type="text" style="display: none;" id="link_{{$i}}"/>
-                                        <button onclick="xemKenh('{{$values[$i][3]}}')" title="{{$values[$i][3]}}" alt="{{$values[$i][3]}}" target="_blank" style="background-color: #2196F3; border: 1px solid #2196F3;  box-shadow: 0 3px 5px -1px rgb(0 0 0 / 20%), 0 6px 10px 0 rgb(0 0 0 / 14%),
+                                        <input value="{{$values[$i][3]??''}}" type="text" style="display: none;" id="link_{{$i}}"/>
+                                        <button onclick="xemKenh('{{$values[$i][3]??''}}')" title="{{$values[$i][3]??''}}" alt="{{$values[$i][3]??''}}" target="_blank" style="background-color: #2196F3; border: 1px solid #2196F3;  box-shadow: 0 3px 5px -1px rgb(0 0 0 / 20%), 0 6px 10px 0 rgb(0 0 0 / 14%),
                                         0 1px 18px 0 rgb(0 0 0 / 12%); padding: 5px 10px; margin: 5px; min-width: 61px;" onclick="copy('link_{{$i}}')">Xem kênh</button>
                                         <button style="background-color: #8CE78C; border: 1px solid #8CE78C; margin: 5px;  box-shadow: 0 3px 5px -1px rgb(0 0 0 / 20%), 0 6px 10px 0 rgb(0 0 0 / 14%),
                                         0 1px 18px 0 rgb(0 0 0 / 12%); padding: 5px 10px; min-width: 61px;" onclick="copy('link_{{$i}}')">Sao chép</button>
@@ -121,7 +121,7 @@
                                         0 1px 18px 0 rgb(0 0 0 / 12%); padding: 5px 10px;">Đặt mua</button>
                                     </td>
                                     <td>
-                                        {{$values[$i][6]}}
+                                        {{$values[$i][6]??''}}
                                     </td>
                                 </tr>
                             @endfor
