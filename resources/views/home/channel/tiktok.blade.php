ec5f0598bf020011836e4c40b8dec3b0bd09ea1f
Nguyễn Thành Lâm <lnguyen24794@gmail.com>
2022-02-24 11:20:01 +0700

    Update tiktok.blade.php

--- resources/views/home/channel/tiktok.blade.php
+++ resources/views/home/channel/tiktok.blade.php
@@ -113,7 +113,6 @@
                                     </td>
 
                                     <td>{{number_format($values[$i][4], 0, ',', '.')}} <br/>
-
                                         <button onclick="go()" style="background-color: #E1CE69; border: 1px solid #E1CE69;  box-shadow: 0 3px 5px -1px rgb(0 0 0 / 20%), 0 6px 10px 0 rgb(0 0 0 / 14%),
                                         0 1px 18px 0 rgb(0 0 0 / 12%); padding: 5px 10px;">Đặt mua</button>
                                     </td>
