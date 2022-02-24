314f82060aaa7eef084eafc8f9f2efe1e2d26b41
Nguyễn Thành Lâm <lnguyen24794@gmail.com>
2022-02-24 11:17:51 +0700

    Update tiktok.blade.php

--- resources/views/home/channel/tiktok.blade.php
+++ resources/views/home/channel/tiktok.blade.php
@@ -84,9 +84,8 @@
                                 <th>{{$values[0][0]??''}}</th>
                                 <th>{{$values[0][1]??''}}</th>
                                 <th>{{$values[0][2]??''}}</th>
-                                <th style="display: none">{{$values[0][3]??''}}</th>
+                                <th>{{$values[0][3]??''}}</th>
                                 <th>{{$values[0][4]??''}}</th>
-                                <th>{{$values[0][7]??''}}</th>
                                 <th>{{$values[0][5]??''}}</th>
                                 <th>{{$values[0][6]??''}}</th>
                             </tr>
