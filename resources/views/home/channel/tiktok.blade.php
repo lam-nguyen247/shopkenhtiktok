06fc3e29d54c952abfbdc73ffbc52ee28bc7565a
Nguyễn Thành Lâm <lnguyen24794@gmail.com>
2022-02-24 11:20:53 +0700

    Update tiktok.blade.php

--- resources/views/home/channel/tiktok.blade.php
+++ resources/views/home/channel/tiktok.blade.php
@@ -102,9 +102,6 @@
                                         {{$values[$i][2]??''}}
                                     </td>
                                     <td>
-                                        {{$values[$i][3]??''}}
-                                    </td>
-                                    <td>
                                         <input value="{{$values[$i][3]??''}}" type="text" style="display: none;" id="link_{{$i}}"/>
                                         <button onclick="xemKenh('{{$values[$i][3]??''}}')" title="{{$values[$i][3]??''}}" alt="{{$values[$i][3]??''}}" target="_blank" style="background-color: #2196F3; border: 1px solid #2196F3;  box-shadow: 0 3px 5px -1px rgb(0 0 0 / 20%), 0 6px 10px 0 rgb(0 0 0 / 14%),
                                         0 1px 18px 0 rgb(0 0 0 / 12%); padding: 5px 10px; margin: 5px; min-width: 61px;" onclick="copy('link_{{$i}}')">Xem kênh</button>
