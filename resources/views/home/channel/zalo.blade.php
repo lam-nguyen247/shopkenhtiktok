a89cd6689f4253e6e689d45142c5e65c5987bbcc
Nguyễn Thành Lâm <lnguyen24794@gmail.com>
2024-01-08 10:58:48 +0700

    Update zalo.blade.php

--- resources/views/home/channel/zalo.blade.php
+++ resources/views/home/channel/zalo.blade.php
@@ -1,58 +1,335 @@
-<!DOCTYPE html>
 <html lang="en">
 
 <head>
-    <meta charset="utf-8" />
-    <link rel="icon" href="https://stc-zaloprofile.zdn.vn/favicon.ico" />
-    <meta name="viewport" content="width=device-width,initial-scale=1" />
-    <meta name="theme-color" content="#000000" />
+    <meta charset="utf-8">
+    <link rel="icon" href="https://stc-zaloprofile.zdn.vn/favicon.ico">
+    <meta name="viewport" content="width=device-width,initial-scale=1">
+    <meta name="theme-color" content="#000000">
     <meta name="description" content="Zalo - Nhắn Gửi Yêu Thương (Nhắn tin thoại - Trò chuyện nhóm ...)">
-    <meta property="og:title" content="Zalo - Lâm Nguyễn" />
-    <meta property="og:description" content="Zalo - Nhắn Gửi Yêu Thương (Nhắn tin thoại - Trò chuyện nhóm ...)" />
-    <meta property="og:image" content="https://qr.zalo.me/sl/2/fc219422836f6a31337e" />
-    <link rel="preconnect" href="https://fonts.googleapis.com" />
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500&display=swap" rel="stylesheet" />
+    <meta property="og:title" content="Zalo - Lâm Nguyễn">
+    <meta property="og:description" content="Zalo - Nhắn Gửi Yêu Thương (Nhắn tin thoại - Trò chuyện nhóm ...)">
+    <meta property="og:image" content="https://qr.zalo.me/sl/2/fc219422836f6a31337e">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500&amp;display=swap" rel="stylesheet">
     <title>Zalo - Lâm Nguyễn</title>
     <script src="https://za.zdn.vn/v3/za.js"></script>
     <script defer="defer" src="https://stc-zaloprofile.zdn.vn/profile/static/js/main.1.0.4.js"></script>
-    <link href="https://stc-zaloprofile.zdn.vn/profile/static/css/main.1.0.4.css" rel="stylesheet" />
+    <link href="https://stc-zaloprofile.zdn.vn/profile/static/css/main.1.0.4.css" rel="stylesheet">
+    <style id="lle-page-public-style">
+        @font-face {
+            font-family: 'Poppins';
+            font-weight: 300;
+            src: url(chrome-extension://eapjofhjnfknfjggkkdnlidaedinfdgb/assets/Poppins-Light.ttf) format('truetype');
+        }
+
+        @font-face {
+            font-family: 'Poppins';
+            src: url(chrome-extension://eapjofhjnfknfjggkkdnlidaedinfdgb/assets/Poppins-Regular.ttf) format('truetype');
+            font-weight: normal;
+        }
+
+        @font-face {
+            font-family: 'Poppins';
+            src: url(chrome-extension://eapjofhjnfknfjggkkdnlidaedinfdgb/assets/Poppins-Medium.ttf) format('truetype');
+            font-weight: 500;
+        }
+
+        @font-face {
+            font-family: 'Poppins';
+            src: url(chrome-extension://eapjofhjnfknfjggkkdnlidaedinfdgb/assets/Poppins-SemiBold.ttf) format('truetype');
+            font-weight: 600;
+        }
+
+
+        @font-face {
+            font-family: 'Byte Sans';
+            font-weight: 300;
+            src: url(chrome-extension://eapjofhjnfknfjggkkdnlidaedinfdgb/assets/ByteSans-semiLight.ttf) format('truetype');
+        }
+
+        @font-face {
+            font-family: 'Byte Sans';
+            src: url(chrome-extension://eapjofhjnfknfjggkkdnlidaedinfdgb/assets/ByteSans-Regular.ttf) format('truetype');
+            font-weight: normal;
+        }
+
+        @font-face {
+            font-family: 'Byte Sans';
+            src: url(chrome-extension://eapjofhjnfknfjggkkdnlidaedinfdgb/assets/ByteSans-Medium.ttf) format('truetype');
+            font-weight: 500;
+        }
+
+        @font-face {
+            font-family: 'Byte Sans';
+            src: url(chrome-extension://eapjofhjnfknfjggkkdnlidaedinfdgb/assets/ByteSans-Bold.ttf) format('truetype');
+            font-weight: 600;
+        }
+
+
+        @font-face {
+            font-family: 'OpenSerif';
+            src: url(chrome-extension://eapjofhjnfknfjggkkdnlidaedinfdgb/assets/OpenSerif-Regular.ttf) format('truetype');
+            font-weight: 500;
+        }
+
+        @font-face {
+            font-family: 'OpenSerif';
+            src: url(chrome-extension://eapjofhjnfknfjggkkdnlidaedinfdgb/assets/OpenSerif-Bold.ttf) format('truetype');
+            font-weight: 600;
+        }
+
+        .lle-variable,
+        .lle-lang-vi,
+        .lle-lang-vi *,
+        .lle-lang-pt,
+        .lle-lang-pt *,
+        .lle-lang-es,
+        .lle-lang-es *,
+        .lle-lang-ja,
+        .lle-lang-ja *,
+        .lle-lang-zh,
+        .lle-lang-zh *,
+        .lle-lang-en,
+        .lle-lang-en * {
+            --n-950: #151719;
+            --n-900: #1f2229;
+            --n-800: #1c1e20;
+            --n-700: #656a73;
+            --n-500: #999da6;
+            --n-400: #c3c6cb;
+            --n-300: #e4e5e7;
+            --n-200: #edeef0;
+            --n-100: #f3f4f6;
+            --n-50: #f8f8fa;
+            --white: #fff;
+            --g-400: #54e79b;
+            --g-500: #00d778;
+            --g-600: #00af62;
+            --g-700: #00925a;
+            --g-800: #00925a;
+            --w-500: #0af;
+            --y-500: #ff961e;
+            --o-500: #fa772a;
+            --r-500: #ff6262;
+            --r-600: #ff6262;
+            --r-700: #d94747;
+            --r-800: #b23232;
+            --b-500: #4984fa;
+            --line-color-border-1: #e7e8ea;
+            --text-text-1: #101019;
+            --light-gray-3: #f7f8f8;
+            --light-gray-10: #061429;
+            --color-fill-4: #d0d3d6;
+            --liner-4: linear-gradient(90deg, #00d778 30.29%, #1ee33e 100%);
+            --warning-1: #fff7e8
+        }
+
+        .lle-lang-zh,
+        .lle-lang-zh * {
+            font-family: PingFang SC, Microsoft Yahei, sans-serif;
+            line-height: 160%
+        }
+
+        .lle-lang-en,
+        .lle-lang-en * {
+            font-family: Byte Sans, sans-serif;
+            line-height: 140%
+        }
+
+        .lle-lang-ja,
+        .lle-lang-ja *,
+        .lle-lang-pt,
+        .lle-lang-pt *,
+        .lle-lang-vi,
+        .lle-lang-vi *,
+        .lle-lang-es,
+        .lle-lang-es * {
+            font-family: Poppins, sans-serif;
+            line-height: 140%
+        }
+
+        .lle-lang-custom-zh,
+        .lle-lang-custom-zh * {
+            font-family: PingFang SC, Microsoft Yahei, sans-serif
+        }
+
+        .lle-lang-custom-en,
+        .lle-lang-custom-en * {
+            font-family: Byte Sans, sans-serif
+        }
+
+        .lle-lang-custom-ja,
+        .lle-lang-custom-ja * {
+            font-family: Poppins, sans-serif
+        }
+
+        .lle-lang-custom-pt,
+        .lle-lang-custom-pt *,
+        .lle-lang-custom-vi,
+        .lle-lang-custom-vi *,
+        .lle-lang-custom-es,
+        .lle-lang-custom-es * {
+            font-family: OpenSerif
+        }
+
+        .lle-translated-text-style {
+            border-left: 2px solid var(--g-500, #00d778);
+            padding-left: 4px
+        }
+    </style>
 </head>
 
-<body>
+<body data-new-gr-c-s-check-loaded="14.1049.0" data-gr-ext-installed="">
     <div id="root">
-        <script>
-            window.pageInfo = {
-                "type": 2,
-                "name": "Mkmedia",
-                "avatar": "",
-                "thumbnail": "",
-                "qrCodeUrl": "",
-                "user": {
-                    "isBusiness": false,
-                    "showBanner": false
-                },
-                "messageInfo": {
-                    "type": "phone",
-                    "argv": "0965590009",
-                    "hasZWebSession": true,
-                    "convId": "1982045621036825276"
-                }
-            };
-        </script>
-        <script>
-            function copyZalo() {
-                /* Get the text field */
-                var copyText = "0965590009";
-                var $temp = $("<input>");
-                $("body").append($temp);
-                $temp.val(copyText).select();
-                document.execCommand("copy");
-                $temp.remove();
-                alert('Đã sao chép số zalo')
-            }
-        </script>
+        <header class="header"><a title="Zalo" href="https://zalo.me/" target="_blank" rel="noreferrer"><img
+                    class="header__logo" src="https://stc-zaloprofile.zdn.vn/profile/assets/zalo-logo.png"
+                    alt="Logo"></a>
+            <ul class="header__list">
+                <li class="relative"><button id="language-menu" class="header__list-item header__language-toggle"
+                        title="Thay đổi ngôn ngữ hiển thị" type="button" aria-haspopup="listbox">Ngôn ngữ:<mark
+                            class="header__highlight">Tiếng Việt<svg xmlns="http://www.w3.org/2000/svg" fill="none"
+                                viewBox="0 0 16 16" width="16" height="16">
+                                <path fill="#0068ff" fill-rule="evenodd"
+                                    d="M3.87 6.282a.6.6 0 0 1 .848-.012L8 9.463l3.281-3.193a.6.6 0 0 1 .837.86l-3.7 3.6a.6.6 0 0 1-.836 0l-3.7-3.6a.6.6 0 0 1-.012-.848Z"
+                                    clip-rule="evenodd"></path>
+                            </svg></mark></button>
+                    <ul class="header__language-menu" role="listbox" tabindex="-1" aria-labelledby="language-menu"
+                        aria-multiselectable="false">
+                        <li role="option" class="header__language-menu-item chosen" aria-selected="true"
+                            data-index="0">Tiếng Việt</li>
+                        <li role="option" class="header__language-menu-item" aria-selected="false" data-index="1">
+                            English</li>
+                    </ul>
+                </li>
+            </ul>
+        </header>
+        <div class="app-content">
+            <main class="section main">
+                <div class="main__info">
+                    <div class="main__profile-name"><span class="main__avatar"><img
+                                src="https://zpsocial-f40-org.zadn.vn/80ce95010671ea2fb360.jpg" alt="Avatar"></span>
+                        <div>
+                            <h1 class="main__name">SOC MEDIA</h1>
+                            <div class="main__profile-type">Liên hệ: 0965590009</div>
+                            <button type="button" class="button medium" title="Nhắn tin qua Zalo" aria-pressed="false"
+                                style="width: 240px;"><svg xmlns="http://www.w3.org/2000/svg" fill="none"
+                                    viewBox="0 0 25 24" width="24" height="24">
+                                    <path fill="#ffffff" fill-rule="evenodd"
+                                        d="M16.2 4.563a8.315 8.315 0 0 0-3.687-.863 8.315 8.315 0 0 0-7.217 4.198 8.294 8.294 0 0 0 .08 8.342c.141.238.198.516.16.79l-.27 1.88v.08a.28.28 0 0 0 .28.24l1.922-.27h.17c.218 0 .432.06.62.17a8.315 8.315 0 0 0 10.452-1.603 8.299 8.299 0 0 0 1.959-7.103A8.295 8.295 0 0 0 16.2 4.563Zm-8.473-.774A9.517 9.517 0 0 1 12.513 2.5a9.518 9.518 0 0 1 7.55 3.753 9.496 9.496 0 0 1-4.017 14.558 9.518 9.518 0 0 1-8.408-.641l-1.882.27h-.21a1.483 1.483 0 0 1-1.472-1.69l.27-1.88A9.493 9.493 0 0 1 7.728 3.789ZM7.739 10a.6.6 0 0 1 .6-.6h6.347a.6.6 0 1 1 0 1.2H8.339a.6.6 0 0 1-.6-.6Zm0 3.5a.6.6 0 0 1 .6-.6h2.933a.6.6 0 1 1 0 1.2H8.34a.6.6 0 0 1-.6-.6Z"
+                                        clip-rule="evenodd"></path>
+                                </svg>Sao chép số Zalo
+                            </button>
+                        </div>
+                    </div>
+                    <div class="profile">
+                        <h2>Giới thiệu bản thân</h2>
+                        <div class="profile__description empty">CÔNG TY CỔ PHẦN CÔNG NGHỆ TRUYỀN THÔNG MK MEDIA.</div>
+                    </div>
+                </div>
+
+            </main>
+        </div>
+
     </div>
-</body>
+
+
+    <div id="lle-shadow-host" data-key="c529b909-17d9-4fdb-ba57-4640ad753e9d"></div>
+    <style id="page-style-page-text-expand">
+        .lle-page-translate-loading {
+            animation: .75s linear infinite lle-circle;
+            display: inline-block;
+            vertical-align: middle !important;
+            border: 2px solid var(--g-500, #00d778) !important;
+            border-top-color: #0000 !important;
+            border-radius: 50% !important;
+            width: 10px !important;
+            height: 10px !important;
+            margin: 0 4px !important;
+            padding: 0 !important
+        }
+
+        .lle-page-translate-retry {
+            cursor: pointer;
+            display: inline-block;
+            vertical-align: middle !important;
+            color: var(--r-500, #ff6262) !important;
+            box-sizing: border-box !important;
+            text-indent: -9999px !important;
+            width: 1em !important;
+            height: 1em !important;
+            margin: 0 4px !important;
+            padding: 0 !important;
+            font-size: 14px !important;
+            position: relative !important
+        }
+
+        .lle-page-translate-retry:before,
+        .lle-page-translate-retry:after {
+            content: "";
+            box-sizing: inherit;
+            position: absolute;
+            top: 50%;
+            left: 50%;
+            transform: translate(-50%, -50%)
+        }
+
+        .lle-page-translate-retry:before {
+            border: 2px solid;
+            border-right-color: #0000;
+            border-radius: 50%;
+            width: .9em;
+            height: .9em
+        }
+
+        .lle-page-translate-retry:after {
+            border: .2em solid;
+            border-color: #0000 currentColor currentColor #0000;
+            top: 20%;
+            left: 75%
+        }
+
+        @keyframes lle-circle {
+            0% {
+                transform: rotate(0)
+            }
+
+            to {
+                transform: rotate(360deg)
+            }
+        }
+
+        .lle-page-highlight {
+            position: relative
+        }
+
+        .lle-page-highlight:before {
+            content: "";
+            background: linear-gradient(180deg, var(--g-500, #00d778)0, #00d77833 2px, #00d77800 100%);
+            animation: 1s 2 forwards lle-highlight;
+            position: absolute;
+            inset: 0
+        }
+
+        @keyframes lle-highlight {
+            0% {
+                opacity: 0
+            }
+
+            50% {
+                opacity: 1
+            }
+
+            to {
+                opacity: 0
+            }
+        }
+
+        .lle-page-clamp-unset {
+            -webkit-line-clamp: unset !important;
+            max-height: unset !important
+        }
+    </style>
+</body><grammarly-desktop-integration data-grammarly-shadow-root="true"></grammarly-desktop-integration>
 
 </html>
